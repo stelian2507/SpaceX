65a6feba8b51a23478807f7d46c4e0f2cc54ba0d
Stelian Grosu <stelian.email@yahoo.com>
2021-02-16 14:58:41 -0500

    s6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 // App.js
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 const element = <h1>Hello, world!</h1>;
 const statement = <h1> React is cool! </h1>;
 function App() {
+  useEffect(() =>{
+    axios.get('https://api.spacexdata.com/v4/launches/latest')
+    .then((res) => {
+console.log(res);
+
+    })
+  },[])
+
   return <div className="App">
  {statement} {element}
   </div>;
