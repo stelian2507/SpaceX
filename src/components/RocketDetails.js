9fac8e9376f68c0c3ab48632073b44404cb6dc45
Rastko <rastkodjurica@yahoo.com>
2021-02-18 15:33:02 -0500

    falconOne

--- src/components/RocketDetails.js
+++ src/components/RocketDetails.js
@@ -6,16 +6,18 @@ function Rocketdetails(props) {
 
     let [Rocketdetails, setRocketdetails] = useState([''])
 
-    // useEffect(() => {
-    //     axios.get('https://api.spacexdata.com/v3/rockets').then((res) => {
-    //         console.log(res.data[0]);
-    //         setRocketdetails(res.data[0]);
-    //     });
-    // }, []);
+    useEffect(() => {
+        axios.get('https://api.spacexdata.com/v3/rockets').then((res) => {
+            console.log(res.data);
+            setRocketdetails(res.data);
+        });
+    }, []);
 
     return (
         <div className='rocketDetails'>
             Falcone One
+            {console.log('Rocket Details =', Rocketdetails?.[0].country)}
+            {Rocketdetails?.[0].country}
             {/* {Rocketdetails.description}{Rocketdetails.engines}
             {Rocketdetails.first_flight}{Rocketdetails.flicker_image[1]} */}
         </div>
