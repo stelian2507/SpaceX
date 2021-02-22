47f87842ca035deac6371eed97bde8aa709a9ee5
Rastko <rastkodjurica@yahoo.com>
2021-02-22 12:06:10 -0500

    almostFinish

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom'
 function Footer(props) {
     return (
         <footer>
-            <Link className="textLink" to="/">Powerd by:</Link>
+            {/* <Link className="textLink" to="/">Powerd by:</Link>
             <Link className="textLink" to="https://www.linkedin.com/in/stelian-grosu">Stelian</Link>
-            <Link className="textLink" to="https://www.linkedin.com/in/rastkodurica">Rastko</Link>
+            <Link className="textLink" to="https://www.linkedin.com/in/rastkodurica">Rastko</Link> */}
+            <p className='footerP'>Power By:
+            <a href='https://www.linkedin.com/in/stelian-grosu'>Stelian</a>
+                <a href='https://www.linkedin.com/in/rastkodurica'>Rastko</a>
+            </p>
         </footer>
     );
 }
